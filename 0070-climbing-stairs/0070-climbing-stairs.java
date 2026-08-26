f41dec7dd188cbb595c962a724037f79ee28b68b
Ayush Katiyar <143071145+ayushkatiyar31@users.noreply.github.com>
2026-08-27 00:50:23 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 42 MB (73.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,15 +5,16 @@ class Solution {
             return 1;
         }
 
-        int prev2 = 1;
-        int prev1 = 1;
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
 
         for(int i = 2; i <= n ;i++){
 
-            int current = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = current;
+           dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return prev1;
+        return dp[n];
     }
 }
