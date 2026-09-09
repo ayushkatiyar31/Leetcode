087152e2490ec12cd92e9adae333771a36cf7d61
Ayush Katiyar <143071145+ayushkatiyar31@users.noreply.github.com>
2026-09-09 15:55:50 +0530

    4245-COUNT-COMMAS-IN-RANGE Stats: Time: 1 ms (83.58%), Space: 42.7 MB (22.43%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.java
+++ 4245-count-commas-in-range/4245-count-commas-in-range.java
@@ -1,5 +1,12 @@
 class Solution {
     public int countCommas(int n) {
-        return Math.max(0, n - 999);
+        
+       int ans = 0;
+
+       for(long x = 1000 ; x <= n; x *= 1000){
+           ans += n - x + 1;
+       }
+       return ans;
+
     }
 }
